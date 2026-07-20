3a6cc8221288c99aadd1c3b23b67ac79399481a5
Mark <88988701+TinyNeuron@users.noreply.github.com>
2026-07-20 01:35:01 -0400

    Update and rename Remove Duplicates from Sorted Array to removeDuplicates.py

--- top-interview-150/removeDuplicates.py
+++ top-interview-150/removeDuplicates.py
@@ -1,5 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        runtime: 0 ms, memory: 20.30 mB
+        """
         nums[:] = list(dict.fromkeys(nums)) # this is the new way of doing it to do it in place.
-        # could do something like this too, nums[:] = sorted(list(set(nums))) 
+        # could do something like this too, 
+        # nums[:] = sorted(list(set(nums))) 
         # nums[:] is the whole list, set() removes duplicates, back to a list(), then sorted() to put it in order
