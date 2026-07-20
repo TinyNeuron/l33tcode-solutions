176dc2efd3688072143b536fcf4fb92cde187f2b
Mark <88988701+TinyNeuron@users.noreply.github.com>
2026-07-20 01:36:40 -0400

    Update removeDuplicates.py

--- top-interview-150/removeDuplicates.py
+++ top-interview-150/removeDuplicates.py
@@ -3,7 +3,7 @@ class Solution:
         """
         runtime: 0 ms, memory: 20.30 mB
         """
-        nums[:] = list(dict.fromkeys(nums)) # this is the new way of doing it to do it in place.
+        nums[:] = list(dict.fromkeys(nums)) # this is the new way of doing it to do it in-place. Keys need to be hashable.
         # could do something like this too, 
         # nums[:] = sorted(list(set(nums))) 
         # nums[:] is the whole list, set() removes duplicates, back to a list(), then sorted() to put it in order
